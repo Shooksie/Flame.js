0d86ad737050d43eab328cb308806fd696197b74
Shooskie <ss9td@mail.umkc.edu>
2018-07-22 14:48:53 -0500

    first Message

---
+++ components.js
@@ -0,0 +1,127 @@
+
+// class Page {
+//   constructor(key) {
+//     // a key is the unique way to specify an entry point in a Page
+//     // the key will allow us to select from the template for the page, the dom
+//     // maniulation point.
+//     // for example the template is:
+//     //    "<div id='key'></div>"
+//     // in this case the div above is all this page has to worry about managing
+//     // this page will then once rendered control key and keep track of it and
+//     // its children in the dom in the
+//     // another example of how key can be used in the template of a Page
+//     // assuming we are using bootstrap 4 for some the css styling
+//     //  <div class='container'>
+//     //  <div class='top-nav'>...insert top nav code here</div>
+//     // </div>
+//     this.template = '<div></div>'
+//     // the template represents the main code for the page,
+//   }
+// }
+
+class Base {
+  constructor(type) {
+    this.children = [];
+    if (type) {
+      this._dom = document.createElement(type);
+      console.log(this._dom);
+    }
+  }
+  _loadefault() {
+    return;
+  }
+  _loadcss(css){ this._dom.style.cssText = css; };
+  _getDom(){ return this._dom; };
+  addChild(child){this._dom.appendChild(child)}
+  setChild(child){
+    this.children.push(child);
+  }
+  renderChildren() {
+    for (let child of this.children) {
+      console.log(child);
+      console.log('appending children');
+      this._dom.appendChild(child._getDom());
+    }
+  }
+};
+
+
+export class Div extends Base {
+  constructor () {
+    const type="div";
+    super(type);
+    this._dom.style.cssText = 'flex: 1; background-color: white;'
+  }
+};
+
+export class Text extends Base {
+  constructor () {
+    const type="p";
+    super(type);
+  }
+  setValue(value) {
+    this._dom.innerText = value;
+  }
+};
+
+export class Table extends Base {
+  constructor () {
+    const type="table";
+    super(type);
+  }
+  createHeader() { return; }
+};
+export class TableHeader extends Base {
+  constructor () {
+    const type="th";
+    super(type);
+  }
+  createHeader() { return; }
+};
+export class TableCell extends Base {
+  constructor () {
+    const type="td";
+    super(type);
+  }
+  createHeader() { return; }
+};
+export class TableRow extends Base {
+  constructor () {
+    const type="tr";
+    super(type);
+  }
+};
+
+export class Button extends Base {
+  constructor () {
+    const type="button";
+    super(type);
+    this._dom.style.textCss = 'flex: 1, background-color: blue;'
+    this._dom.innerHTML = 'click me bitch'
+  }
+
+  onClick(handler) {
+    this._dom.onclick = handler;
+  }
+};
+
+export class Component {
+  constructor() {
+    this._dom = document.createElement('div')
+    this._dom.style.cssText = 'display: flex; flex: 1, background-color: black'
+  }
+  _getDom() { return this._dom;}
+  _renderComponent() {
+    console.log('rendering the component');
+    console.log(this);
+    const obj = this.render()
+    console.log('rendering wrapper');
+    console.log(obj);
+    this._dom.appendChild(obj._getDom());
+    obj.renderChildren();
+  }
+
+  render() {
+    return;
+  }
+}
---
+++ spark.js
@@ -0,0 +1,73 @@
+
+let exports = {}
+// we will use specfic keys to pre-define specfic html elements
+// | key | type      | html equivelent         | attirbutes                      |
+// | div | container | <div></div>             | all current html params         |
+// | text| text      | any text html equivelent| a type param and all text params|
+// |     |           |                         |
+// Note: pages by nature will render in a row fashion they will be tweakable to
+// render children in a side by side fashion
+//
+// class myCustomComponent extends component {
+//
+//     triggerState() {
+//       this.store.number += 1;
+//     }
+//     setup() {
+//       this.store.number = 0;
+//     }
+//     renderComponent() {
+//       let textV = Text(this.store.number);
+//       let children = [
+//         {
+//           type: 'div',
+//           class: 'card rounded'
+//           children: [
+//
+//           ]
+//         }
+//       ]
+//       return children
+//     }
+// }
+// class pageX extends Page {
+//    key='#pageKey'
+//    template='html template'
+//    renderPage() {
+//         let header = new myHeaderComponent()
+//         let myComponent = new myCustomComponent()
+//         let myComponent2 = new mySecondComponent()
+//         let Button = new myCustomButton({
+//           onClick: myComponent.triggerState,
+//         })
+//         return([header,myComponent,myComponent2,Button]);
+//    }
+// }
+//
+// const sparkInstace = new Spark('#startingpoint')
+//                          .addChild(pageX)
+//                          .addChild(pageY)
+//                          .enableSideNav() <- function will be used to create
+//                          .enableTopNav()                    the side nav
+export class Spark {
+  constructor(selector) {
+    this.virtual_dom = {};
+    this.entryPoint(selector);
+  }
+  entryPoint(selector) {
+    this.virtual_dom.parent = select(selector)
+    const { parent } = this.virtual_dom;
+    parent.style.cssText = "display: flex; background-color: red, width: 100; height: 100%;";
+  }
+  addChild(child) {
+    // function that adds a child into the virtual_dom
+    // each child will have access to its own component/page dom
+    // this.virtual_dom[child.key] = child.render();
+    const { parent } = this.virtual_dom;
+    parent.appendChild(child._getDom());
+    child._renderComponent();
+  }
+}
+
+
+const select = (selector) => { return document.querySelector(selector)}
