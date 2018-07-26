7ba042459dabb9aec4d440119f3e89303484d53d
Shooskie <ss9td@mail.umkc.edu>
2018-07-25 21:17:32 -0500

    fragmentation render improvements

--- src/Views/testviews.js
+++ src/Views/testviews.js
@@ -1,58 +1,60 @@
 import { Component, Text, Div, Button } from '../../lib';
 
-export default class TestView extends Component {
+const Styles = {
+  MainDiv: {
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  DivOne: {
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+}
+
+
+class ButtonView extends Component {
+  constructor(args) {
+    super();
+    this.args = args;
+  }
+  render() {
+      const { valueOne, valueTwo} = this.args;
+      const ButtonValue= new Button();
+      const TextValue = new Text();
+
+      TextValue.setValue(valueOne);
+      ButtonValue.onClick(()=> {
+        if(TextValue._getDom().innerText === valueOne) {
+          TextValue.setValue(valueTwo);
+        }
+        else {
+          TextValue.setValue(valueOne);
+        }
+      });
+
+      return (new Div({
+          children: [TextValue, ButtonValue],
+          style: Styles.DivOne
+        })
+      );
+  }
+}
+
+export default class Main extends Component {
   constructor() {
     super();
   }
   render() {
-    const DivOne = new Div();
-    const DivTwo = new Div();
-    const MainDiv = new Div();
-    DivOne.loadcss({
-      textAlign: 'center',
-      justifyContent: 'center',
-      alignItems: 'center'
-    })
-    MainDiv.loadcss({
-      textAlign: 'center',
-      justifyContent: 'center',
-      alignItems: 'center'
-    })
-
-    const ButtonClick = new Button();
-    const Button2 = new Button();
-
-    const TextValue = new Text();
-    const TextValue1 = new Text();
-
-    TextValue.setValue('hey');
-    ButtonClick.onClick(()=> {
-      if(TextValue._getDom().innerText === 'whats up') {
-        TextValue.setValue('hey');
-      }
-      else {
-        TextValue.setValue('whats up');
-      }
-    });
-
-    TextValue1.setValue('oh yeah');
-    Button2.onClick(()=> {
-      if(TextValue1._getDom().innerText === 'oh yeah') {
-        TextValue1.setValue('daym');
-      }
-      else {
-        TextValue1.setValue('oh yeah');
-      }
-    });
-
-    DivOne.setChild(TextValue);
-    DivOne.setChild(ButtonClick)
-
-    DivTwo.setChild(TextValue1);
-    DivTwo.setChild(Button2);
-
-    MainDiv.setChild(DivOne);
-    MainDiv.setChild(DivTwo);
-    return MainDiv;
+    const ButtonView1 = new ButtonView({valueOne: 'whatup', valueTwo: 'no way'});
+    const ButtonView2 = new ButtonView({valueOne: 'hey man', valueTwo: 'whatup'});
+    return (new Div({
+        children: [
+          ButtonView1, ButtonView2
+        ],
+        style: Styles.MainDiv
+      })
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import { flame } from '../lib';
-import TestView from './Views/testviews';
+import Main from './Views/testviews';
 import './styles.css';
 
 
 function component() {
-  var element = document.createElement('div');
-  var spark = new flame('body').addChild(new TestView());
-  return element;
+  return (new flame('body',Main));
 }
 
 let element = component(); // Store the element to re-render on print.js changes
-document.body.appendChild(element);
-
 if (module.hot) {
    module.hot.accept('../lib', function() {
-     console.log('Accepting the updated printMe module!');
-     document.body.removeChild(element);
+     document.body.innerHTML = ""
      element = component(); // Re-render the "component" to update the click handler
-     document.body.appendChild(element);
    })
  }
